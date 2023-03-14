aabb891ead7c6d412ec7740c2b989717d9a9e5c4
Rita2022 <rita.ferreira2529@gmail.com>
2023-03-14 09:56:02 -0300

    inserindo exemplo de incrementar

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,20 @@
+<script setup lang="ts">
+import {ref}from 'vue';
+const nome = ref("Nome");
+const contador =ref(1)
+function incrementar(){
+  contador.value++
+}
+
+</script>
+
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Bem Vindo{{ nome }}</h1>
+    <p><input type="text" v-model="nome"/></p>
+    <p v-if="nome.length> 5">Nome longo</p>
+    <p v-else>nome curto</p> 
+   <p> {{contador}} <button @click="incrementar">Botão</button></p>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
